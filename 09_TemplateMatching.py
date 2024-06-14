a14e49b4a1682ad2ee8787b744157978e556d0d4
Navid Hejazi <navid.hejazi@Tensator.com>
2024-06-14 15:41:54 +0100

    Work on image histograms

--- 09_TemplateMatching.py
+++ 09_TemplateMatching.py
@@ -10,8 +10,9 @@ w,h = template.shape
 
 result = cv.matchTemplate(image, template, cv.TM_CCOEFF_NORMED)
 
-threshold = 0.6
+threshold = 0.7
 locations = np.where(result >= threshold)
+
 for point in zip(*locations[::-1]):
     cv.rectangle(image, point, (point[0] + h, point[1] + w), (255,255,0), 2)
 
